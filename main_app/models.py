d3e1cffba124d41ad3d46f7ad34b9069bba432bd
Signal-flow12 <tcravenny@gmail.com>
2023-07-04 14:49:04 -0400

    Teams table created in psql

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Teams(models.Model):
+    name = models.CharField(max_length=150)
+    img = models.CharField(max_length=300)
+    conference = models.CharField(max_length=25)
+    superbowlwins = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,23 @@ class Home(TemplateView):
     
 class About(TemplateView):
     template_name = 'about.html'
+
+class  Teams:
+    def __init__(self, name, image, conference):
+        self.name = name
+        self.image = image
+        self.conference = conference
+
+teams = [
+    Teams("Raiders", "https://pbs.twimg.com/profile_images/1611220466743783429/gg0OPgEw_400x400.jpg", "AFC"),
+    Teams("Eagles", "https://1000logos.net/wp-content/uploads/2017/05/Philadelphia-Eagles-Logo.png", "NFC"),
+    Teams("Giants", "https://s.yimg.com/cv/apiv2/default/nfl/20190724/500x500/2019_NYG_wbg.png", "NFC")
+]
+
+class TeamsList(TemplateView):
+    template_name = "teams_list.html"
+
+    def get_context_data(self, ** kwargs):
+        context = super().get_context_data(**kwargs)
+        context["teams"] = teams
+        return context
