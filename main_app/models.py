d70b623bdbb09b33d2392f5a8bd55e51a58c2923
Signal-flow12 <tcravenny@gmail.com>
2023-07-04 21:13:59 -0400

    Added players relashionship

--- main_app/models.py
+++ main_app/models.py
@@ -14,3 +14,16 @@ class Team(models.Model):
     class Meta:
         ordering = ['name']
 
+
+
+class Players(models.Model):
+
+    playername = models.CharField(max_length=150)
+    postion = models.CharField(max_length=100)
+    points = models.IntegerField()
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="players")
+
+    def __str__(self):
+        return self.playername
+
+
