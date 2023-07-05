79084fb13571e1495443beb4293a8601a90ad073
Signal-flow12 <tcravenny@gmail.com>
2023-07-04 20:51:27 -0400

    Fixed migration issue

--- db/create_teams.sql
+++ db/create_teams.sql
@@ -1,15 +1,15 @@
-INSERT INTO main_app_team(name, img, conference, superbowlwins)
+INSERT INTO main_app_team(name, img, conference, superbowlwins, created_at)
   VALUES
-    ('Raiders', 'https://pbs.twimg.com/profile_images/1611220466743783429/gg0OPgEw_400x400.jpg','AFC WEST','3');
+    ('Raiders', 'https://pbs.twimg.com/profile_images/1611220466743783429/gg0OPgEw_400x400.jpg','AFC WEST','3', '2021-05-07 18:57:42.517205-07');
 
-INSERT INTO main_app_team(name, img, conference, superbowlwins)
+INSERT INTO main_app_team(name, img, conference, superbowlwins, created_at)
   VALUES
-    ('Cheifs','https://logos-world.net/wp-content/uploads/2020/05/Kansas-City-Chiefs-logo.png','AFC WEST','3');
+    ('Cheifs','https://logos-world.net/wp-content/uploads/2020/05/Kansas-City-Chiefs-logo.png','AFC WEST','3', '2021-05-07 18:57:42.517205-07');
 
-INSERT INTO main_app_team(name, img, conference, superbowlwins)
+INSERT INTO main_app_team(name, img, conference, superbowlwins, created_at)
   VALUES
-    ('Broncos','https://logos-world.net/wp-content/uploads/2020/05/Denver-Broncos-logo.png','AFC WEST', '3');
+    ('Broncos','https://logos-world.net/wp-content/uploads/2020/05/Denver-Broncos-logo.png','AFC WEST', '3', '2021-05-07 18:57:42.517205-07');
 
-INSERT INTO main_app_team(name, img, conference, superbowlwins)
+INSERT INTO main_app_team(name, img, conference, superbowlwins, created_at)
   VALUES
-    ('Chargers','https://logos-world.net/wp-content/uploads/2020/06/Los-Angeles-Chargers-Logo.png','AFC WEST','0');
+    ('Chargers','https://logos-world.net/wp-content/uploads/2020/06/Los-Angeles-Chargers-Logo.png','AFC WEST','0', '2021-05-07 18:57:42.517205-07');
--- main_app/models.py
+++ main_app/models.py
@@ -1,14 +1,16 @@
 from django.db import models
-
 # Create your models here.
+
 class Team(models.Model):
     name = models.CharField(max_length=150)
     img = models.CharField(max_length=300)
-    conference = models.CharField(max_length=25)
-    superbowlwins = models.CharField(max_length=15)
+    conference = models.CharField(max_length=30)
+    superbowlwins = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
     
     class Meta:
         ordering = ['name']
+
