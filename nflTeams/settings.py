0679cc43ef5a3481fe2b90e3daa9c7222604cce1
Signal-flow12 <tcravenny@gmail.com>
2023-07-07 11:25:46 -0400

    installed apps update

--- nflTeams/settings.py
+++ nflTeams/settings.py
@@ -43,7 +43,6 @@ if RENDER_EXTERNAL_HOSTNAME:
 # Application definition
 
 INSTALLED_APPS = [
-    'render.apps.config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
