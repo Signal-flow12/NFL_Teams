a93b23e4e880a690f6853998ad2f717e30c5d2da
Signal-flow12 <tcravenny@gmail.com>
2023-07-11 20:16:18 -0400

    Static files dirs

--- nflTeams/settings.py
+++ nflTeams/settings.py
@@ -92,16 +92,16 @@ WSGI_APPLICATION = 'nflTeams.wsgi.application'
 
 # To use Neon with Django, you have to create a Project on Neon and specify the project connection settings in your settings.py in the same way as for standalone Postgres.
 
-DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'fantasy_football',
-    'USER': os.environ['DB_USER'],
-    'PASSWORD': os.environ['DB_PW'],
-    'HOST': os.environ['DB_HOST'],
-    'PORT': '5432',
-  }
-}
+# DATABASES = {
+#   'default': {
+#     'ENGINE': 'django.db.backends.postgresql',
+#     'NAME': 'fantasy_football',
+#     'USER': os.environ['DB_USER'],
+#     'PASSWORD': os.environ['DB_PW'],
+#     'HOST': os.environ['DB_HOST'],
+#     'PORT': '5432',
+#   }
+# }
 
 
 # Database
